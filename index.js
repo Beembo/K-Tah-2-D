ffeaebe53177ea8c783eea917e840f0d62401578
Beembo <43397021+Beembo@users.noreply.github.com>
2018-12-14 03:39:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function drawBackground(img) {
 }
 
 function draw() {
-  background("rgba(40, 70, 150, 10)");
+  background("rgba(255, 0, 0, 0.2)");
   player.draw();
   player.move({ x: mouseX, y: mouseY }, 0.05);
   enemies.forEach(enemy => enemy.draw());

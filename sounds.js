d0ba3654a95f78621fc55517dad0ab761db32438
Beembo <43397021+Beembo@users.noreply.github.com>
2018-12-14 22:50:19 -0800

    Update sounds.js
    Updated with the source link for game-over sound.

--- sounds.js
+++ sounds.js
@@ -1,6 +1,7 @@
 //SOURCE: https://www.youtube.com/watch?v=NFjE5A4UAJI
 let backgroundMusic = new Audio("https://raw.githubusercontent.com/Beembo/K-Tah-2-D/master/ChaserSounds/jotarosTheme.mp3");
 
+//SOURCE: https://www.youtube.com/watch?v=NYjZnI9_kg8
 let video = document.createElement("video");
 video.setAttribute(
   "src",

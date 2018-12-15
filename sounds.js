902deb153d8dba0d36f5be7db2d96a4805334e24
Beembo <43397021+Beembo@users.noreply.github.com>
2018-12-14 17:31:24 -0800

    Update sounds.js
    Tidied and added GameOver game sound from CodePen.

--- sounds.js
+++ sounds.js
@@ -1,2 +1,8 @@
 //SOURCE: https://www.youtube.com/watch?v=NFjE5A4UAJI
 let backgroundMusic = new Audio("https://raw.githubusercontent.com/Beembo/K-Tah-2-D/master/ChaserSounds/jotarosTheme.mp3");
+
+let video = document.createElement("video");
+video.setAttribute(
+  "src",
+  "https://raw.githubusercontent.com/Beembo/K-Tah-2-D/master/ChaserGameOver//DIO's%20Insane%20Laugh.mp4"
+);
